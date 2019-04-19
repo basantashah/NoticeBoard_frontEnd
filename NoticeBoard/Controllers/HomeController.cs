8484db7f9c00c447887a8035957aac500027c15d
basantashah <bindassbasanta@hotmail.com>
2019-04-19 17:02:04 +0545

    adding dotnet for development

---
+++ NoticeBoard/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NoticeBoard.Models;
+
+namespace NoticeBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Loggedin()
+        {
+            return View();
+        }
+
+        public IActionResult NoticeList()
+        {
+            return View();
+        }
+
+        public IActionResult PostMessages()
+        {
+            return View();
+        }
+
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
