1dc9de7d7b5c53d83d7a106da1b046da9e380a91
basantashah <bindassbasanta@hotmail.com>
2019-05-25 00:01:43 +0545

    adding changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,12 @@
-
-
-const server='https://27e95fe8.ngrok.io/'
+const server = "https://5391db4d.ngrok.io/";
 const config = {
-    
-    login: server+'api/user/login',
-    register:server+'api/auth/register',
-    notice:server+'api/notice/fetch',
-    yourNotice:server+'api/notice/yournotice',
-    postNotice:server+'api/notice/post',
-    updateNotice:server+'api/notice/update',
-    deleteNotice:server+'api/notice/delete'
- 
+  login: server + "api/user/login",
+  register: server + "api/auth/register",
+  notice: server + "api/notice/fetch",
+  yourNotice: server + "api/notice/yournotice",
+  postNotice: server + "api/notice/post",
+  updateNotice: server + "api/notice/update",
+  deleteNotice: server + "api/notice/delete"
 };
- 
-export default config;
 
+export default config;
